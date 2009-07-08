d551aa890b73ed71939f2968f9e8d5ca5f0184eb
Stanislaw Kolarzowski <stasiokol@poczta.onet.pl>
2009-07-09 01:43:01 +0200

    Add Node model

--- lib/btree.rb
+++ lib/btree.rb
@@ -1,4 +1,58 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+#== btree_class
+#Stanislaw Kolarzowski 2009
+#
+#Implemention of B-tree
+#    * create B-tree
+#    * insert values
+#    * delete values
+#    * searching values
+#    * upper iteration
+#    * set order(n)
 
-puts "Hello World"
+#In computer science, a B-tree is a tree data structure that keeps data sorted
+#and allows searches, insertions, and deletions in logarithmic amortized time.
+#Unlike self-balancing binary search trees, it is optimized for systems that read
+#and write large blocks of data. It is most commonly used in databases and filesystems.
+#more: http://en.wikipedia.org/wiki/B-tree
+
+# See README file
+
+
+  class Tree
+    attr_reader :n
+    def initialize(options = {})
+      @n = options[:n] || 5
+    end
+  end
+
+  class Node
+    attr_reader :items, :parent
+
+    def initialize(parent = 0, values = nil)
+      @parent = parent
+      @items = if values then values.sort else [] end
+    end
+
+    def size
+      @items.size
+    end
+
+    def left
+      @items.first
+    end
+
+    def right
+      @items.last
+    end
+
+    def add(element)
+      unless @items.include?(element)
+        @items << element
+        @items.sort!
+      end
+    end
+
+    def delete(element)
+      @items.delete(element)
+    end
+  end
---
+++ test/test_btree.rb
@@ -0,0 +1,37 @@
+require "test/unit"
+require "lib/btree.rb"
+
+class TestBTree < Test::Unit::TestCase
+  def test_node_initialize
+    root_parent = Node.new
+    five_parent = Node.new(5)
+    empty = Node.new
+    unsort = Node.new(0, [5, 2, 7, 11, 9])
+    
+    assert_equal 0, root_parent.parent
+    assert_equal 5, five_parent.parent
+    assert_equal [], empty.items
+    assert_equal [2, 5, 7, 9, 11], unsort.items
+  end
+
+  def test_node_actions
+    node = Node.new(0, [11, 12, 14])
+    assert_equal 3, node.size
+    assert_equal 11, node.left
+    assert_equal 14, node.right
+
+    node.add(14)
+    assert_equal 3, node.size
+    node.add(13)
+    assert_equal 4, node.size
+    assert_equal 14, node.right
+
+    node.delete(10)
+    assert_equal 4, node.size
+    node.delete(14)
+    assert_equal 3, node.size
+  end
+end
+
+
+
